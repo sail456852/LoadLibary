0e2f79d9765af29b53d33b8fbdd9639af7ef78b1
wangyuzhen <sail456852@aol.com>
2016-07-20 23:32:42 +0800

    Update dllsource.cpp

--- dllsource.cpp
+++ dllsource.cpp
@@ -2,7 +2,7 @@
 
 //dll function ready to be exported
 
-extern "C" int __declspec(dllexport) __stdcall  myfunction()
+extern "C" int __declspec(dllexport) __stdcall  myfunction()  //without extern "C" ,this function won't be located from the host application
 {
 	std::cout << "I am being called from the dll dynamically!";
 	return 1;
