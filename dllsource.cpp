acd3ba293a0db1591f02b16f7100534b6f8ab577
wangyuzhen <sail456852@aol.com>
2016-08-09 09:17:13 +0800

    Update dllsource.cpp

--- dllsource.cpp
+++ dllsource.cpp
@@ -7,3 +7,10 @@ extern "C" int __declspec(dllexport) __stdcall  myfunction()  //without extern "
 	std::cout << "I am being called from the dll dynamically!";
 	return 1;
 }
+
+//During my test , I found that there is no need to add __stdcall , and there is no need to add __declspec(dllexport) before a function 
+//definition , if you can do  that before the function declaration in the corresponding header file , :P 
+
+//and also there is no need to use the header file ,but you can use it , it doesn't really matter , if you just wanna call a function in that
+//dll , why ? because calling the function in that dll from exe process , is done indirectly ,through function pointer . you can't 
+//call that function directly through the function name , because that name is in another namespace . 
